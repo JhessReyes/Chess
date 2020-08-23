cd6a644dac99f22c81e86650a918bbf59df98932
JhessReyes <64570196+JhessReyes@users.noreply.github.com>
2020-08-22 21:26:47 -0600

    v1

---
+++ Movimientos.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $("#a7").click(function(event) {
+        $("#pe_n1").remove();
+        $("#a5").prop("#pe_n1");
+    });
+});
+
+window.onload = function() {
+    document.getElementById('tit').onclick = function () {
+        alert('NO HAY NADA'); 
+    }
+}
