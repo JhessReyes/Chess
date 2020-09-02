d46c1e32a8ebdbb1f576b99d683e7ddce78d0120
JhessReyes <64570196+JhessReyes@users.noreply.github.com>
2020-09-02 06:48:34 -0600

    Interface

--- Movimientos.js
+++ Movimientos.js
@@ -1,13 +1,37 @@
 
-$(document).ready(function() {
-    $("#a7").click(function(event) {
-        $("#pe_n1").remove();
-        $("#a5").prop("#pe_n1");
-    });
-});
+function play(){
+    Matriz();
+    alert("Hola");
+}
 
-window.onload = function() {
-    document.getElementById('tit').onclick = function () {
-        alert('NO HAY NADA'); 
+var bloqueselec_x;
+var bloqueselec_y;
+var board = new Array(8);
+
+function Selectblock(x,y){
+    board[x][y] = 1;
+    PintarBloque(x, y,"aquamarine");
+}
+
+function Matriz(){
+    for(i=0;i<8;i++) board[i] = new Array(8);
+    Limpiar();
+    Selectblock(x,y);
+}
+function Limpiar(){
+    for(x=0; x<8; x++){
+        for(y=2; y<6; y++){
+            board[x][y]=0;
+        }
     }
 }
+function CheckBloque(x,y){
+    Selectblock(x,y);
+}
+
+function PintarBloque (x,y, color){
+    Bloque = document.getElementById("b"+x+y);
+    Bloque.style.background = color;
+}
+
+play();
